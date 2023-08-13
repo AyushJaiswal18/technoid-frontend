61f763f928963bb5c0f70df5ad8fee8e952fc04b
L-Pircy <taveesha.agarwal@gmail.com>
2023-08-13 14:48:12 +0530

    bg and logo in assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Feed from "./views/Feed/Feed";
 import Login from "./views/Login/Login";
 import PreLoginLanding from "./views/PreLoginLanding/PreLoginLanding";
@@ -14,13 +19,13 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-        <Route exact path="/" element={<PreLoginLanding/>} />
-        <Route exact path ="/feed" element={<Feed/>} /> 
-        <Route exact path ="/login" element={<Login/>} /> 
-        <Route exact path ="/profile" element={<Profile/>} /> 
-        <Route exact path ="/signup" element={<Signup/>} /> 
-        <Route exact path ="/ticket" element={<Ticket/>} /> 
-        <Route exact path ="/ticket-creation" element={<TicketCreation/>} />
+          <Route exact path="/" element={<PreLoginLanding />} />
+          <Route exact path="/feed" element={<Feed />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/ticket" element={<Ticket />} />
+          <Route exact path="/ticket-creation" element={<TicketCreation />} />
         </Routes>
       </Router>
     </div>
