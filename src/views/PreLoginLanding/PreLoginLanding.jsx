210150e4214f428597bba22c6b65e2951f5155a8
L-Pircy <taveesha.agarwal@gmail.com>
2023-08-18 21:25:27 +0530

    landing page responsive

--- src/views/PreLoginLanding/PreLoginLanding.jsx
+++ src/views/PreLoginLanding/PreLoginLanding.jsx
@@ -15,54 +15,33 @@ const PreLoginLanding = () => {
         style={{ backgroundImage: `url(${backgroundImage})` }}
       >
         {/* Navigation Bar */}
-        {/* <Nav /> */}
-        <NavbarDefault/>
-        {/* <nav className="w-full bg-opacity-75 bg-transparent">
-          <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex items-center justify-between h-22">
-              <div className="mr-4 mt-2">
-                <img src={logoIEEE} alt="Logo" className="w-[150px] h-[45px]" />
-              </div>
-              <div className="flex space-x-8 mr-10">
-                <a
-                  href="#"
-                  className="text-[#eeeeee] text-lg hover:text-gray-300"
-                >
-                  HOME
-                </a>
-                <a
-                  href="#"
-                  className="text-[#eeeeee] text-lg hover:text-gray-300"
-                >
-                  SERVICES
-                </a>
-                <a href="#" className="text-white text-lg hover:text-gray-300">
-                  CONTACT US
-                </a>
-                <a href="#" className="text-white text-lg hover:text-gray-300">
-                  ABOUT
-                </a>
-              </div>
-            </div>
-          </div>
-        </nav> */}
+        <NavbarDefault />
 
         <div className="flex flex-col items-center justify-center h-full">
           <div className="w-2/3">
-            <h2 className="flex justify-center text-4xl">
+            <h2
+              className="flex justify-center 
+            md:text-4xl
+            sm:text-3xl
+            text-2xl"
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
               lorem.
             </h2>
           </div>
           <div className="space-x-4 mt-8">
             <button
-              className="bg-[#eeeeee] text-black text-md py-2 px-[3.3rem] rounded-full border border-black"
+              className="bg-[#eeeeee] text-black rounded-full border border-black
+              sm:text-md sm:py-2 sm:px-[3.3rem] 
+              text-sm py-[6px] px-[1.8rem]"
               onClick={() => navigate("/login")}
             >
               LOG IN
             </button>
             <button
-              className="bg-[#C72F55] text-[#eeeeee] text-md px-[3.3rem] py-2 rounded-full"
+              className="bg-[#C72F55] text-[#eeeeee] rounded-full
+              sm:text-md sm:px-[3.3rem] sm:py-2 
+              text-sm py-[6px] px-[1.8rem]"
               onClick={() => navigate("/signup")}
             >
               SIGN UP
