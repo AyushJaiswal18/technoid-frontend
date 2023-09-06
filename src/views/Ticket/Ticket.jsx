9fa20bf0a3b1735ab2b3ab0282f576baa939bc7c
L-Pircy <taveesha.agarwal@gmail.com>
2023-09-06 22:01:03 +0530

    code check

--- src/views/Ticket/Ticket.jsx
+++ src/views/Ticket/Ticket.jsx
@@ -1,30 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navbar from "../../components/Navbar";
 import backgroundImage from "../../assets/bgfeed.png";
 import Footer from "../../components/Footer";
 import TicketCard from "./TicketCard";
 
-// async function fetchrepo(name) {
-//   const response = await fetch("https://api.github.com/l-pircy");
-//   const repo = await response.json();
-//   return repo;
-// }
-
-const Ticket = async () => {
-  const [tickets, setTickets] = useState([]);
-
-  const fetchData = () => {
-    return fetch("https://api.github.com/l-pircy")
-      .then((response) => response.json())
-      .then((data) => setTickets(data));
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-  // const repo = await fetchrepo();
-  // console.log(repo);
-
+const Ticket = () => {
   return (
     <div
       className="bg-center bg-[#1F1F1E] flex flex-col text-white"
